ab65c05edf9dacc8401fc2b9ba2346ae150820c5
zelttikDevv <alberrttoop@gmail.com>
2026-06-28 00:32:39 -0500

    Create relationship.ts

---
+++ data/relationship.ts
@@ -0,0 +1,6 @@
+export const relationshipData = {
+  startDate: new Date('2026-06-06T00:00:00'),
+  person1: 'Alberto',
+  person2: 'Cristal',
+  subtitle: 'Los recuerdos no viven en los chats. Viven en nosotros.',
+};
